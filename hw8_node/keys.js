b2ff815d13b29197d608c1eeabd07bf533e7a4ba
Nick <nlweber3@gmail.com>
2018-02-01 22:15:11 -0800

    commit

---
+++ hw8_node/keys.js
@@ -0,0 +1,14 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.JuOAcCOg261NEOE8r5ZleDnlo,
+  consumer_secret: process.env.uV8A1gimjSEeA7kSAD19c2tUYFMPLRiFv2YCMvvivo1dUXCmi1,
+  access_token_key: process.env.959296953426182146-Hr5lSUhgCcvzuv31tkYAaEYhVIkZUug
+  access_token_secret: process.env.T7njgeoDOl5A6T5HW5UdDQZY0Ai57pfCMVf367gBn6uwZ
+};
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
+
